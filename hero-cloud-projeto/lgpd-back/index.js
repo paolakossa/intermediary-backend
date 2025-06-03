fb531a029a194a01ea36053bfd5c5f9daff46d42
Paola <paolakossa@gmail.com>
2025-06-03 15:19:10 -0300

    Ajuste no index do backend

--- hero-cloud-projeto/lgpd-back/index.js
+++ hero-cloud-projeto/lgpd-back/index.js
@@ -3,12 +3,15 @@ import { pkg } from "body-parser";
 import { router } from "./routes/router.js";
 import sequelize from "./utils/database.js";
 import association from "./models/Associations.js";
+import cors from "cors";
 
 const app = express();
 const { json, urlencoded } = pkg;
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
+app.use("/", router);
+app.use(cors());
 
 (async () => {
   try {
@@ -22,4 +25,4 @@ app.use(urlencoded({ extended: true }));
   }
 })();
 
-app.use("/", router);
+
