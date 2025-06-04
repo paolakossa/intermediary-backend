ba8448a8c97a2a0c9e25d5fbcf330cba713c6f07
Paola <paolakossa@gmail.com>
2025-06-04 16:33:39 -0300

    Ajustes finais

--- hero-cloud-projeto/lgpd-back/routes/UserController.js
+++ hero-cloud-projeto/lgpd-back/routes/UserController.js
@@ -26,17 +26,26 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single("profile_picture");
 
 router.post("/addUser", async (req, res) => {
-  const userModel = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    gender: req.body.gender,
-    profile_picture: req.file ? req.file.path : null,
-  };
-  const user = await userService.saveUser(userModel);
-  return res.status(201).json({
-    message: "User created successfully",
-    user: user,
+  upload(req, res, async (err) => {
+    const userModel = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      gender: req.body.gender,
+      profile_picture: req.file ? req.file.path : null,
+    };
+    if (err) {
+      return res.status(500).json({
+        message: "Error uploading file",
+        error: err.message,
+      });
+    }
+
+    const user = await userService.saveUser(userModel);
+    return res.status(201).json({
+      message: "User created successfully",
+      user: user,
+    });
   });
 });
 
@@ -77,24 +86,52 @@ router.delete("/deleteUser/:id", async (req, res) => {
 });
 
 router.put("/updateUser/:id", async (req, res) => {
-  const id = req.params.id;
-  const userModel = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    gender: req.body.gender,
-    profile_picture: req.file ? req.file.path : null,
-  };
-  const updatedUser = await userService.updateUserById(id, userModel);
-  if (!updatedUser) {
-    return res.status(404).json({
-      message: "User not found or no changes made",
+  upload(req, res, async (err) => {
+    const id = req.params.id;
+    const userModel = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      gender: req.body.gender,
+      profile_picture: req.file ? req.file.path : null,
+    };
+    if (err) {
+      return res.status(500).json({
+        message: "Error uploading file",
+        error: err.message,
+      });
+    }
+    const updatedUser = await userService.updateUserById(id, userModel);
+    if (!updatedUser) {
+      return res.status(404).json({
+        message: "User not found or no changes made",
+      });
+    }
+    return res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser,
     });
-  }
-  return res.status(200).json({
-    message: "User updated successfully",
-    user: updatedUser,
   });
 });
+
+router.get("/userImage/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await userService.getUserById(id);
+  if (!user || !user.profile_picture) {
+    return res.status(404).json({
+      message: "User or profile picture not found",
+    });
+  }
+  
+  res.sendFile(process.cwd() + "/" + user.profile_picture, (err) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Error retrieving image",
+        error: err.message,
+      });
+    }
+  });
+}
+);
 
 export default router;
